c46e815e81ec516855d21c9a49968fc02addf240
malcolm worthy <malworthy@gmail.com>
2023-11-11 21:22:07 +1100

    use mongodb

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,46 +1,53 @@
 import { json, error } from "@sveltejs/kit"
 import type { RequestEvent } from "./$types"
-//import Database from 'better-sqlite3';
-//const db = new Database('data/wfh.db')
-//db.pragma('journal_mode = WAL');
+import {
+  MONGO_CS,
+ } from '$env/static/private';
+import { MongoClient } from "mongodb";
+const client = new MongoClient(MONGO_CS);
 
-export async function GET({ url, locals, platform }: RequestEvent) {
+
+export async function GET({ url, locals }: RequestEvent) {
   const session = await locals.getSession()
   if (!session?.user) {
     throw error(401, "You are not signed in.")
   }
-  // create schema
-  try {
-    const stmt = platform?.env.DB.prepare("create table dates(day integer, month integer, year integer, user text, fyear integer)");
-    await stmt.run();
-  } catch {}
-
-  const fyear = url.searchParams.get('fyear');
-  const stmt2 = platform?.env.DB.prepare("select day, month, year from dates where user = ? and fyear = ?");
-  const result = await stmt2.all(session.user.email, fyear);
+  const fyear = Number(url.searchParams.get('fyear'));
+  const database = client.db('myFirstDatabase');
+  const dates = database.collection('dates');
+  const result = dates.find( {user: session.user.email, fyear: fyear });
+  const arr = await result.toArray();
+  //console.log(arr);
  
-  return json(result);
+  return json(arr);
 }
 
-export async function POST({request, locals, platform}) {
+export async function POST({request, locals }) {
   const session = await locals.getSession()
   if (!session?.user) {
     throw error(401, "You are not signed in.")
   }
 	const data = await request.json();
 
-  const stmt1 = platform?.env.DB.prepare("select count(*) as c from dates where day=? and month=? and year=? and user=?");
-  const row:any = await stmt1.get(data.day, data.month, data.year, session.user.email);
+  const document = {
+    day: Number(data.day),
+    month: Number(data.month),
+    year: Number(data.year),
+    fyear: Number(data.fyear),
+    user: session.user.email
+  };
+  const database = client.db('myFirstDatabase');
+  const dates = database.collection('dates');
 
-  const sql = row.c > 0 ? "delete from dates where day=? and month=? and year=? and user=? and fyear = ?" 
-  : "insert into dates values(?,?,?,?,?)";
+  const doc = await dates.findOne(document);
+  if (!doc)
+    await dates.insertOne(document);
+  else
+    await dates.deleteOne(document);
 
-  const stmt2 = platform?.env.DB.prepare(sql);
-  await stmt2.run(data.day, data.month, data.year, session.user.email, data.fyear);
-
-  const stmt3 = platform?.env.DB.prepare("select day, month, year from dates where user = ? and fyear = ?");
-  const result = await stmt3.all(session.user.email, data.fyear);
+  const result = dates.find( {user: session.user.email, fyear: Number(data.fyear) });
+  const arr = await result.toArray();
   
-	return json(result);
+	return json(arr);
 }
 
