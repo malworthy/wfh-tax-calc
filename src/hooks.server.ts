532457325976c234754f1e83d419c8fe08fb74db
malcolm worthy <malworthy@gmail.com>
2023-11-14 06:59:06 +1100

    debugging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,6 +5,8 @@ import {
  GOOGLE_CLIENT_SECRET,
 } from '$env/static/private';
 
+console.log(`ClientId:${GOOGLE_CLIENT_ID}`);
+
 export const handle = SvelteKitAuth({
  providers: [GoogleProvider({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })]
 });
